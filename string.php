dba702c1f79672d0ef851d62acd44ff69ba1d4cc
HweeVaa <gnlqkgnlgus@gmail.com>
2022-01-05 11:16:48 +0900

    first commit

---
+++ string.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h1>String & String Operator</h1>
+    <?php echo "Hello \"w\"orld";?>
+    <h2>connection operator</h2>
+    <?php echo "hello"."world";?>
+    <h2>String length function</h2>
+    <?php echo strlen("hello world");?>
+
+</body>
+
+</html>
